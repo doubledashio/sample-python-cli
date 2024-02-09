7bb988f846585872c9a55ee6a057bda5674c62c5
Mathieu Duchesneau <mathieu@doubledash.io>
2024-02-09 09:20:16 +0000

    update README.md, comments/flow in main.py

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# sample-python-cli
-Dracal // SDK code sample for Python on CLI
+# sample-node-vcp
+Dracal // SDK code sample for Node on CLI
+
+## Assumptions
+
+Running this repository requires you to have installed:
+- Python (version >= `3.x`)
+- DracalView (version >= `3.2.x`)
+  - Specifically, `dracal-usb-get` needs to be accessible from your `PATH` environment variable (more info in the [documentation how-to](https://www.dracal.com/en/programmers_howto/#dracal-usb-get)).
+
+Script may need to be adjusted depending on your instrument's # of outputs _(currently assumed: 3 outputs)_. See script comments for details.
+
+## Simple usage
+
+Run script
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 import subprocess
 
-# Note: dracal-usb-get assumed to be in the path
+# Note: dracal-usb-get assumed to be in the **path**
 # Arguments passed to -i (0,1,2) here need to be updated to fit
 # your scenario. You may also specify a serial number by adding
 # the -s argument.
@@ -10,9 +10,7 @@ import subprocess
 # If dracal-usb-get exits with a non-zero values, the subprocess.CalledProcessError
 # exception will be raised. Catch it.
 try:
-    path = "../dracalview/client/build/bin/"
-    p = subprocess.check_output([path + "dracal-usb-get","-i","0,1"]).decode('utf-8')
-    # p = "44.18, 22.35, 50.12"
+    p = subprocess.check_output(["dracal-usb-get","-i","0,1,2"]).decode('utf-8')
 except subprocess.CalledProcessError:
     print("dracal-usb-get error")
     sys.exit(1)
@@ -45,4 +43,3 @@ print("Pressure..(kPa):", pressure)
 print("Temperature (F):",temperature*9/5+32)
 
 sys.exit(0)
-
